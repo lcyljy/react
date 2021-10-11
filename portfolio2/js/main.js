3121aad0f2b818b282b460b97b5b89b665757a9c
lcyljy <35355087+lcyljy@users.noreply.github.com>
2021-10-12 01:20:13 +0900

    update 2
    header 고정. nav바 눌렀을 때 해당 위치로 스크롤

--- portfolio2/js/main.js
+++ portfolio2/js/main.js
@@ -1,1 +1,10 @@
-console.log("A")
+let boxArr = ["a", "b", "c", "d"]
+for (let i = 0; i < boxArr.length; i++) {
+  document.querySelectorAll(`.${boxArr[i]}Box`)[0].addEventListener("click", function () {
+
+    const location = document.querySelectorAll(`.${boxArr[i]}Box`)[1].getBoundingClientRect().top + window.pageYOffset;
+
+    window.scrollTo({ top: Number(location), behavior: 'smooth' })
+  })
+}
+
