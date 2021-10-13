311ba52088a913540906db44d3566b316ef45483
lcyljy <35355087+lcyljy@users.noreply.github.com>
2021-10-13 18:10:16 +0900

    update 3

---
+++ portfolio3/js/main.js
@@ -0,0 +1,13 @@
+// setInterval(function(){
+//     let checkPoint = document.querySelector(".active a").innerText
+// if(checkPoint == "point"){
+//     document.querySelectorAll("#fp-nav ul li a")[1].firstChild.classList.add("bg-white")
+// }
+// }, 200)
+// // setInterval(function(){
+// // console.log(document.querySelector('.active a').innerText)
+// // }, 1000)
+
+// console.log(document.querySelectorAll("#fp-nav ul li a")[1].firstChild)
+// document.write(document.querySelectorAll("#fp-nav ul li a")[1].firstChild.classList.add("bg-white"))
+// console.log(document.querySelectorAll("#fp-nav ul li a")[0].firstChild)
